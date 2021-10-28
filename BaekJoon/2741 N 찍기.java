f6514e65d39bc0ec21128d611cbf6f45c30072f3
seongeun <seongeunk102@gmail.com>
2021-10-28 11:39:54 +0900

    https://www.acmicpc.net/problem/2741

---
+++ BaekJoon/2741 N 찍기.java
@@ -0,0 +1,16 @@
+package baejoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2741 {
+
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for(int i=1;i<=num;i++){
+            System.out.println(i);
+        }
+    }
+}
